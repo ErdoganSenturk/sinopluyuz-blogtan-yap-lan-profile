6561df9dcbeb75b1312270bf73e32f5ac5a047d3
ErdoganSenturk <101876323+ErdoganSenturk@users.noreply.github.com>
2024-05-24 22:58:46 +0300

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY ='Ssdfhfd4567468CRsfghfET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =  ['.vercel.app']
 
 
 # Application definition
